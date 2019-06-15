e6035092330546a884250406b3b3c770f54ea82b
Amine <abenaceur12@gmail.com>
2019-06-14 21:48:52 -0600

    Updates

---
+++ precache-manifest.1c513d5d22d9f7fe01828b3c9038686b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6822e86e6feedb06866520b92b62af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6d3794f2c66858be667",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "2fb2ac2f6675bdda2b62",
+    "url": "/static/css/main.5b2b7441.chunk.css"
+  },
+  {
+    "revision": "b6d3794f2c66858be667",
+    "url": "/static/js/2.e7c2be2a.chunk.js"
+  },
+  {
+    "revision": "2fb2ac2f6675bdda2b62",
+    "url": "/static/js/main.a97dfddb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7d4afcf6f4e23fad389fa314f9b091ce",
+    "url": "/static/media/ABDevDesign.7d4afcf6.png"
+  },
+  {
+    "revision": "2002a74872d73625a9cfd16d4b488efd",
+    "url": "/static/media/ABLogo.2002a748.png"
+  },
+  {
+    "revision": "77bc92c62ef359d2b33baef1cd701ec8",
+    "url": "/static/media/AminoAvatar.77bc92c6.png"
+  },
+  {
+    "revision": "901c8230d47d94e7bf6408fbe9e356a2",
+    "url": "/static/media/BugIcon.901c8230.jpg"
+  },
+  {
+    "revision": "1145d22c6ebbfa31c69756d79f8ea43a",
+    "url": "/static/media/ContactPhoto.1145d22c.jpg"
+  },
+  {
+    "revision": "8d0000efca09f1f554628acfec96768d",
+    "url": "/static/media/FullStack.8d0000ef.png"
+  },
+  {
+    "revision": "f14dfc6372d1f9a2132eb83064dfb8ae",
+    "url": "/static/media/ProfilePhoto.f14dfc63.png"
+  }
+]);
